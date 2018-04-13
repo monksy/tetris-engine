8ae5dd8ddc9d58559b89dc1af9e08870d8a44adb
Steven <crashoverride025@gmail.com>
2018-04-13 16:42:38 -0500

    Merge line implementation.

--- src/main/scala/com/mrmonksy/drw/tetrisengine/ArrayOperations.scala
+++ src/main/scala/com/mrmonksy/drw/tetrisengine/ArrayOperations.scala
@@ -4,23 +4,56 @@ object ArrayOperations {
 
   /**
     * This checks to see if the two arrays had overlap.
+    *
     * @param boardOffset The board offset where the piece is looking to go.
-    * @param board The board row representitive.
-    * @param compare The block piece to compare against the board row.
-    * @param position Position that we're going to examine.
+    * @param board       The board row representitive.
+    * @param compare     The block piece to compare against the board row.
+    * @param position    Position that we're going to examine.
     * @tparam T The type of the board.
     * @return Returns true if the pieces would not be able to fit. False if they're good
     */
-  def hasConflict[T](boardOffset:Int, board:Array[T], compare: Array[T])(position:Int): Boolean = {
-    !canMerge(compare(position), board(boardOffset+position))
+  def hasConflict[T](boardOffset: Int, board: Array[T], compare: Array[T])(position: Int): Boolean = {
+    !canMerge(compare(position), board(boardOffset + position))
   }
 
   /**
     * This verifies that there is room at the instnace where you can merge.
-    * @param left Left value
+    *
+    * @param left  Left value
     * @param right Right value
     * @tparam T Type that is used.
     * @return Returns true if there is a gap in one of the values.
     */
-  def canMerge[T](left: T, right:T) = left == null || right == null
+  def canMerge[T](left: T, right: T) = left == null || right == null
+
+  /**
+    * This merges the piece into the src array. This is a dangerous merge. Also, if there is an item that fails the merge, the array will remain modified up until the failure.
+    *
+    * THIS METHOD IS D*A*N*G*E*R*O*U*S It will modify the src array.
+    *
+    * @param src   Source of the array to modify.
+    * @param piece The piece to merge in.
+    * @param boardOffset
+    * @param preMergeCheck
+    * @tparam T The type of the value being stored in the board.
+    * @return Returns the reference to the src. Note this is a modified array.
+    *
+    */
+  def mergeLine[T](src: Array[T], piece: Array[T], boardOffset: Int, preMergeCheck: (T, T) => Boolean): Array[T] = {
+    (boardOffset to (boardOffset + piece.length-1)).foreach(pos => {
+      val left = src(pos)
+      val piecePos = pos - boardOffset
+      val right = piece(piecePos)
+
+      if (preMergeCheck(left, right)) {
+        src.update(pos, right)
+      } else {
+        throw new IllegalArgumentException(s"The piece (${piece.mkString(",")}) could not be written into the array at $pos/$piecePos due to a conflict with the following values ($left, $right)")
+      }
+    }
+    )
+
+    src
+  }
+
 }
--- src/test/scala/com/mrmonksy/drw/tetrisengine/ArrayOperationsTest.scala
+++ src/test/scala/com/mrmonksy/drw/tetrisengine/ArrayOperationsTest.scala
@@ -9,48 +9,68 @@ class ArrayOperationsTest extends WordSpec with Matchers {
       val expected = false
       val actual = ArrayOperations.canMerge(true, true)
 
-      actual should be (expected)
+      actual should be(expected)
     }
     "pass when both values are open" in {
       val expected = true
       val actual = ArrayOperations.canMerge(null, null)
 
-      actual should be (expected)
+      actual should be(expected)
     }
     "pass when the left side is open, the right isn't" in {
       val expected = true
       val actual = ArrayOperations.canMerge('A, null)
 
-      actual should be (expected)
+      actual should be(expected)
     }
     "pass when the left side is used, and the right is open" in {
       val expected = true
       val actual = ArrayOperations.canMerge(null, 'B)
 
-      actual should be (expected)
+      actual should be(expected)
     }
   }
 
   "ArrayOperations::hasConflicts" should {
     "Have a conflict on the first item" in {
       val expected = true
-      val board = Array[Integer](1,1,null)
+      val board = Array[Integer](1, 1, null)
       val boardOffset = 1
-      val piece = Array[Integer](1,1)
+      val piece = Array[Integer](1, 1)
 
-      val actual = ArrayOperations.hasConflict(boardOffset, board)(piece, 0)
+      val actual = ArrayOperations.hasConflict(boardOffset, board, piece)(0)
 
-      actual should be (expected)
+      actual should be(expected)
     }
     "Not have a conflict where both items are blank" in {
       val expected = false
-      val board = Array[Integer](1,1,null)
+      val board = Array[Integer](1, 1, null)
       val boardOffset = 1
-      val piece = Array[Integer](1,1)
+      val piece = Array[Integer](1, 1)
 
-      val actual = ArrayOperations.hasConflict(boardOffset, board)(piece, 1)
+      val actual = ArrayOperations.hasConflict(boardOffset, board, piece)(1)
 
-      actual should be (expected)
+      actual should be(expected)
     }
   }
+
+  "ArrayOperations::mergeLine" should {
+    "Merge the items in without any validation issues" in {
+      val board = Array[Integer](1, null, null, 1)
+      val piece = Array[Integer](2, 2)
+      val expected = Array[Integer](1, 2, 2, 1)
+      val actual = ArrayOperations.mergeLine(board, piece, 1, (l: Integer, r: Integer) => true)
+
+      actual should be(expected)
+    }
+
+    "Fail to merge and catch the exception" in {
+      val board = Array[Integer](1, null, null, 1)
+      val piece = Array[Integer](2, 2)
+
+      the[IllegalArgumentException] thrownBy ArrayOperations.mergeLine(board, piece, 1, (l: Integer, r: Integer) => false)
+    }
+  }
+
+
 }
